36879586d4a3192b3528090365aef0502c5e7717
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-12-02 17:18:06 +0530

    add note model

---
+++ backend/src/models/note.model.js
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+
+const noteSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    teamId: {
+        type: Schema.Types.ObjectId,
+        ref: "Team",
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+export const Note = mongoose.model("Note", noteSchema);
