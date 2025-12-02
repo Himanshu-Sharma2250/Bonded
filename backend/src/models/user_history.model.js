449edefa955c08fc235815dd1e59bc1910d91561
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-12-02 17:32:37 +0530

    add user history model

---
+++ backend/src/models/user_history.model.js
@@ -0,0 +1,21 @@
+import mongoose, {Schema} from "mongoose";
+import { availableUserActions, UserAction } from "../constant";
+
+const userHistorySchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    userAction: {
+        type: String,
+        enum: availableUserActions,
+        default: UserAction.JOINED,
+        required: true
+    },
+    reason: {
+        type: String,
+    }
+}, { timestamps: true })
+
+export const UserHistory = mongoose.model("UserHistory", userHistorySchema)
