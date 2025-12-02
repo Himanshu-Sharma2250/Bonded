f4b2dc35c919868723a8e7a2ecf382c5f5d4a801
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-12-02 17:00:36 +0530

    add team model

---
+++ backend/src/models/team.model.js
@@ -0,0 +1,23 @@
+import mongoose, {Schema} from "mongoose";
+
+const teamSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+export const Team = mongoose.model("Team", teamSchema);
