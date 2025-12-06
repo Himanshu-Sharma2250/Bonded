576d976627e770c3d6250d66caa65a0d70a47017
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-12-06 17:37:09 +0530

    add editNote controller

--- backend/src/controllers/note.controller.js
+++ backend/src/controllers/note.controller.js
@@ -44,3 +44,46 @@ export const createNote = async (req, res) => {
     }
 }
 
+// edit note
+export const editNote = async (req, res) => {
+    const {data, error} = editNoteSchema.safeParse(req.body);
+    const {noteId} = req.params;
+
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Error in req body"
+        })
+    }
+
+    const {title, description, isPrivate} = data;
+
+    try {
+        const note = await Note.findByIdAndUpdate(noteId, {
+            title: title,
+            description: description,
+            isPrivate: isPrivate
+        }, {new: true})
+
+        if (!note) {
+            return res.status(404).json({
+                success: false,
+                message: "Note not edited"
+            })
+        }
+
+        await note.save();
+
+        res.status(201).json({
+            success: false,
+            message: 'Note edit successfully',
+            note
+        })
+    } catch (error) {
+        console.error("Error editing note: ", error);
+        res.status(500).json({
+            success: false,
+            message: "Error editing note"
+        })
+    }
+}
