02e24d1381844c0650f1e2ed234f782e587eab09
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-12-07 17:49:19 +0530

    add teamMemberJoined controller

--- backend/src/controllers/team_history.controller.js
+++ backend/src/controllers/team_history.controller.js
@@ -111,4 +111,39 @@ export const teamMemberKickedOut = async (req, res) => {
 }
 
 // member joined
+export const teamMemberJoined = async (req, res) => {
+    const {teamHistoryId} = req.params;
+    const {data, error} = teamHistorySchema.safeParse(req.body);
+
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Error in req body"
+        })
+    }
+
+    const {reason} = data;
+
+    try {
+        const history = await TeamHistory.findByIdAndUpdate(teamHistoryId, {
+            teamAction: "JOINED",
+            reason: reason
+        }, {new: true})
+
+        await history.save()
+
+        res.status(201).json({
+            success: true,
+            message: "Team history updated",
+            history
+        })
+    } catch (error) {
+        console.error("Error in updating team history ", error);
+        res.status(500).json({
+            success: false,
+            message: "Error in updating team history"
+        })
+    }
+}
+
 // team deleted
