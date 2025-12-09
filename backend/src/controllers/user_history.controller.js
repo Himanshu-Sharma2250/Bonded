2ff052cd965a8362e1dfc8efdc2bb6f263b44b72
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-12-09 18:54:21 +0530

    add userJoinedTeam controller

--- backend/src/controllers/user_history.controller.js
+++ backend/src/controllers/user_history.controller.js
@@ -72,3 +72,38 @@ export const userCreatedTeam = async (req, res) => {
         } 
 }
 
+export const userJoinedTeam = async (req, res) => {
+    const {userHistoryId} = req.params;
+    const {data, error} = userHistorySchema.safeParse(req.body);
+    
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Error in req body"
+        })
+    }
+    
+    const {reason} = data;
+
+    try {
+        const history = await UserHistory.findByIdAndUpdate(userHistoryId, {
+            userAction: "JOINED",
+            reason: reason
+        }, {new: true})
+    
+        await history.save()
+        
+        res.status(201).json({
+            success: true,
+            message: "User joined team history",
+            history
+        })
+        } catch (error) {
+            console.error("Error in creating user_joined_team history ", error);
+            res.status(500).json({
+                success: false,
+                message: "Error in creating user_joined_team history"
+            })
+        } 
+}
+
