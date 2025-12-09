dc7e1f8df3e587a43ae8d7c813521a2095843932
Himanshu-Sharma2250 <him221901250@gmail.com>
2025-12-09 18:58:22 +0530

    add userKickedOutOfTeam controller

--- backend/src/controllers/user_history.controller.js
+++ backend/src/controllers/user_history.controller.js
@@ -58,7 +58,7 @@ export const userCreatedTeam = async (req, res) => {
     
         await history.save()
         
-        res.status(201).json({
+        res.status(200).json({
             success: true,
             message: "User created team history",
             history
@@ -93,7 +93,7 @@ export const userJoinedTeam = async (req, res) => {
     
         await history.save()
         
-        res.status(201).json({
+        res.status(200).json({
             success: true,
             message: "User joined team history",
             history
@@ -128,7 +128,7 @@ export const userLeftTeam = async (req, res) => {
     
         await history.save()
         
-        res.status(201).json({
+        res.status(200).json({
             success: true,
             message: "User left team history",
             history
@@ -138,6 +138,41 @@ export const userLeftTeam = async (req, res) => {
             res.status(500).json({
                 success: false,
                 message: "Error in creating user_left_team history"
+            })
+        } 
+}
+
+export const userKickedOutTeam = async (req, res) => {
+    const {userHistoryId} = req.params;
+    const {data, error} = userHistorySchema.safeParse(req.body);
+    
+    if (error) {
+        return res.status(400).json({
+            success: false,
+            message: "Error in req body"
+        })
+    }
+    
+    const {reason} = data;
+
+    try {
+        const history = await UserHistory.findByIdAndUpdate(userHistoryId, {
+            userAction: "KICKED_OUT",
+            reason: reason
+        }, {new: true})
+    
+        await history.save()
+        
+        res.status(200).json({
+            success: true,
+            message: "User kicked out of team history",
+            history
+        })
+        } catch (error) {
+            console.error("Error in creating user_kickedOut_team history ", error);
+            res.status(500).json({
+                success: false,
+                message: "Error in creating user_kickedOut_team history"
             })
         } 
 }
