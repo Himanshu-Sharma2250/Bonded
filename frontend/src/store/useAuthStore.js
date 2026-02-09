72de02be747f0f9cad606c3dc24be4eab7b42f61
Himanshu-Sharma2250 <him221901250@gmail.com>
2026-02-09 17:27:26 +0530

    add useAuthStore
    manage global state for authentication

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,104 @@
+import {create} from "zustand"
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+    user: null,
+    loading: false,
+
+    signUp: async (signUpData) => {
+        set({loading: true});
+
+        try {
+            await axiosInstance.post("/auth/register", signUpData);
+        } catch (error) {
+            console.error("Error registering user: ", error);
+        } finally {
+            set({loading: false});
+        }
+    },
+
+    verifyEmail: async (token) => {
+        try {
+            await axiosInstance.get(`/auth/verify-email/${token}`);
+        } catch (error) {
+            console.error("Error verifying user: ", error);
+        }
+    },
+
+    login: async (loginData) => {
+        set({loading: true});
+
+        try {
+            const res = await axiosInstance.post("/auth/login", signUpData);
+            const data = res.data;
+
+            set({user: data.user});
+        } catch (error) {
+            console.error("Error during user login: ", error);
+        } finally {
+            set({loading: false});
+        }
+    },
+
+    profile: async () => {
+        try {
+            await axiosInstance.get(`/auth/profile`);
+            
+            return true;
+        } catch (error) {
+            console.error("Error accessing user's profile: ", error);
+            return false;
+        }
+    },
+
+    logout: async () => {
+        set({loading: true});
+
+        try {
+            await axiosInstance.post("/auth/logout");
+
+            set({user: null});
+        } catch (error) {
+            console.error("Error during user logout: ", error);
+        } finally {
+            set({loading: false});
+        }
+    },
+
+    forgotPassword: async (emailData) => {
+        try {
+            await axiosInstance.post("/auth/forgot-password", emailData);
+            return true;
+        } catch (error) {
+            console.error("Error during forgotPassword");
+            return false;
+        }
+    },
+
+    resetPassword: async (token, passwordData) => {
+        set({loading: true});
+
+        try {
+            const res = await axiosInstance.post(`/auth/reset-password/${token}`, passwordData);
+            const data = res.data;
+
+            set({user: data.user});
+        } catch (error) {
+            console.error("Error reseting password: ", error);
+        } finally {
+            set({loading: false})
+        }
+    },
+
+    refreshToken: async () => {
+        try {
+            const res = await axiosInstance.post("/auth/refresh-token");
+            
+            set({user: res.data.user});
+        } catch (error) {
+            console.error("Error refreshing token: ", error);
+        }
+    },
+
+    
+}))
